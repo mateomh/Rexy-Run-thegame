2de3cb3c377b0b6c5f50dc801f63848876d3d863
mateomh <mmojicah.84@gmail.com>
2020-11-04 12:35:05 -0500

    Create test for the title scene

--- tests/unit_tests/preloader.test.js
+++ tests/unit_tests/preloader.test.js
@@ -1,7 +1,7 @@
 import * as Phaser from 'phaser';
 import PreloaderScene from '../../src/scenes/preloader';
 
-test('1. Test boot scene inheritance from phaser ', () => {
+test('1. Test preloader scene inheritance from phaser ', () => {
   const testScene = new PreloaderScene();
   expect(testScene instanceof Phaser.Scene).toBeTruthy();
 });
