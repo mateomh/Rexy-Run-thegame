51644a1652d20365757af9141111d14620eb4cdf
mateomh <mmojicah.84@gmail.com>
2020-11-05 16:16:24 -0500

    Add test to the preloader test file

--- tests/unit_tests/preloader.test.js
+++ tests/unit_tests/preloader.test.js
@@ -1,7 +1,16 @@
 import * as Phaser from 'phaser';
 import PreloaderScene from '../../src/scenes/preloader';
+import TitleScene from '../../src/scenes/title-scene';
+import gameConfig from '../../src/config/gameconfig';
+
+const game = new Phaser.Game(gameConfig);
 
 test('1. Test preloader scene inheritance from phaser ', () => {
   const testScene = new PreloaderScene();
   expect(testScene instanceof Phaser.Scene).toBeTruthy();
+});
+
+test('2. Test if the scene was added to the game', () => {
+  game.scene.add('Preload', PreloaderScene);
+  expect(game.scene.getScenes.length).toBe(2);
 });
